8dc2ebb1872bf94ca253c0b69d3b2500ac6d076c
Dylan Smith <dylantsmith@gmail.com>
2024-09-15 17:56:29 -0500

    test: add jest
    - add jest
    - add a single test, for middleware

---
+++ jest.config.ts
@@ -0,0 +1,32 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+  collectCoverageFrom: [
+    'src/**/*.ts*',
+    // exclude everything but api from /app
+    '!src/app/**',
+    'src/app/api/**',
+    // exclude all these directories and everything below them
+    '!src/config/**',
+    '!src/lib/fakes/**',
+    '!src/lib/schemas/**',
+    '!src/lib/scratch-data/**',
+    '!src/scripts/**',
+    '!src/types/**',
+  ],
+  coveragePathIgnorePatterns: ['src/components/*', 'src/lib/logger.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: [],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config);
