87df2696f98ff0c455ed42430ab196c9e9260258
Dylan Smith <dylantsmith@gmail.com>
2024-09-22 15:11:35 -0500

    feat: input component + storybook
    - add the input component
    - add support for storybook
    - add a story for the input component

---
+++ src/components/stories/ui/input.stories.tsx
@@ -0,0 +1,16 @@
+import { Input } from '@/components/ui/input';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof Input> = {
+  args: {
+    type: 'text',
+  },
+  component: Input,
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Default: Story = {
+  args: {},
+};
