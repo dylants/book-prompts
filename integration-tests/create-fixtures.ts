9bae19c11996faf3254c3a486ee9a06173f81710
Dylan Smith <dylantsmith@gmail.com>
2024-09-28 09:21:31 -0500

    feat: login auth API
    - add an API to authenticate the user and return an auth cookie, containing the user's UUID.
    - add an integration test for login

--- integration-tests/create-fixtures.ts
+++ integration-tests/create-fixtures.ts
@@ -1,12 +1,14 @@
+import userService from '@/lib/services/user.service';
 import { PrismaClient } from '@prisma/client';
 import bookReviewFixtures from './fixtures/book-review.fixture';
 import userFixtures from './fixtures/user.fixture';
 const prisma = new PrismaClient();
 
 async function createFixtures() {
-  await prisma.user.createMany({
-    data: userFixtures,
-  });
+  // use user service to make sure we add user's correctly
+  for (const user of userFixtures) {
+    await userService.addUser({ user });
+  }
 
   // the first user gets the book reviews
   const user = await prisma.user.findFirstOrThrow();
