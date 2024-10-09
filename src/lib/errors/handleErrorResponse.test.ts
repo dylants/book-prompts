9340bb89ff655c796fe09f89a796c03eda1aeca3
Dylan Smith <dylantsmith@gmail.com>
2024-10-09 17:18:53 -0500

    feat: zod API POST schema validation
    - add zod schema validation for the login route
    - simplify the error handling in the login route, and other routes by using `handleErrorResponse`. Include `BadRequestError` to the list of errors handled.

--- src/lib/errors/handleErrorResponse.test.ts
+++ src/lib/errors/handleErrorResponse.test.ts
@@ -1,7 +1,18 @@
+import { BadRequestError } from '@/lib/errors/BadRequestError';
 import { handleErrorResponse } from '@/lib/errors/handleErrorResponse';
 import { UnauthorizedError } from '@/lib/errors/UnauthorizedError';
 
 describe('handleErrorResponse', () => {
+  it('should return 400 for bad request', async () => {
+    const response = handleErrorResponse(
+      new BadRequestError('you did the wrong thing!'),
+    );
+    expect(response.status).toEqual(400);
+    expect(await response.json()).toEqual({
+      error: 'you did the wrong thing!',
+    });
+  });
+
   it('should return 401 for unauthorized', async () => {
     const response = handleErrorResponse(new UnauthorizedError());
     expect(response.status).toEqual(401);
--- src/lib/errors/handleErrorResponse.ts
+++ src/lib/errors/handleErrorResponse.ts
@@ -1,13 +1,23 @@
+import { BadRequestError } from '@/lib/errors/BadRequestError';
 import { UnauthorizedError } from '@/lib/errors/UnauthorizedError';
+import logger from '@/lib/logger';
 import NextResponseErrorBody from '@/types/NextResponseErrorBody';
 import { NextResponse } from 'next/server';
 
 export function handleErrorResponse(
   error: unknown,
 ): NextResponse<NextResponseErrorBody> {
-  if (error instanceof UnauthorizedError) {
+  if (error instanceof BadRequestError) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  } else if (error instanceof UnauthorizedError) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   } else {
+    let errorMessage;
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    }
+    logger.error({ errorMessage }, 'Unknown error');
+
     return NextResponse.json({ error: 'Unknown error' }, { status: 500 });
   }
 }
