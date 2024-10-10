f8e94e790dab274353fed2b94f8944cb8b5623e4
Dylan Smith <dylantsmith@gmail.com>
2024-10-10 18:54:27 -0500

    feat: input variants
    - add error and ghost variants

--- src/components/stories/ui/input.stories.tsx
+++ src/components/stories/ui/input.stories.tsx
@@ -14,3 +14,11 @@ type Story = StoryObj<typeof Input>;
 export const Default: Story = {
   args: {},
 };
+
+export const Error: Story = {
+  args: { variant: 'error' },
+};
+
+export const Ghost: Story = {
+  args: { variant: 'ghost' },
+};
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,17 +1,33 @@
 import { cn } from '@/lib/tailwind-utils';
+import { cva, VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+const inputVariants = cva(
+  'flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-slate-950 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    defaultVariants: {
+      variant: 'default',
+    },
+    variants: {
+      variant: {
+        default: '',
+        error: 'border-red-500',
+        ghost: 'border-none shadow-none focus-visible:ring-transparent',
+      },
+    },
+  },
+);
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement>,
+    VariantProps<typeof inputVariants> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, variant, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          'flex h-9 w-full rounded-md border border-slate-200 bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-slate-950 placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-950 disabled:cursor-not-allowed disabled:opacity-50',
-          className,
-        )}
+        className={cn(inputVariants({ className, variant }))}
         ref={ref}
         {...props}
       />
