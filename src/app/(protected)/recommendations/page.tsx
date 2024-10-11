f6ca08476c661d89ad8da20a25947a05e617e952
Dylan Smith <dylantsmith@gmail.com>
2024-10-11 17:13:35 -0500

    feat: book recommendations page (#27)
    - add the book recommendations page, which calls the POST recommendations API to generate recommendations

---
+++ src/app/(protected)/recommendations/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import BookRecommendation from '@/components/recommendations/BookRecommendation';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import useHandleError from '@/hooks/useHandleError';
+import { postRecommendations } from '@/lib/api';
+import Recommendation from '@/types/Recommendation';
+import { BookCopyIcon } from 'lucide-react';
+import { useCallback, useState } from 'react';
+
+export default function RecommendationsPage() {
+  const [recommendations, setRecommendations] = useState<Array<Recommendation>>(
+    [],
+  );
+  const { handleError } = useHandleError();
+
+  const generateRecommendations = useCallback(async () => {
+    try {
+      const generatedRecommendations = await postRecommendations();
+      setRecommendations(generatedRecommendations);
+    } catch (error) {
+      return handleError(error);
+    }
+  }, [handleError]);
+
+  return (
+    <div>
+      <h1 className="flex gap-2 items-center">
+        <BookCopyIcon size={18} />
+        Recommendations
+      </h1>
+      {recommendations.length === 0 ? (
+        <div className="flex flex-col w-full items-center mt-[80px] gap-3">
+          <p>Generate personalized book recommendations</p>
+          <Button variant="default" onClick={() => generateRecommendations()}>
+            Generate
+          </Button>
+        </div>
+      ) : (
+        <div className="mt-10 grid gap-8">
+          <Separator />
+          {recommendations.map((recommendation) => (
+            <div key={recommendation.title} className="grid gap-8">
+              <BookRecommendation recommendation={recommendation} />
+              <Separator />
+            </div>
+          ))}
+          <div className="flex mt-4 w-full justify-center">
+            <Button variant="default" onClick={() => generateRecommendations()}>
+              Regenerate
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ src/components/recommendations/BookRecommendation.tsx
@@ -0,0 +1,47 @@
+import Recommendation from '@/types/Recommendation';
+import {
+  BookIcon,
+  MessageCircleQuestionIcon,
+  SparklesIcon,
+} from 'lucide-react';
+
+export default function BookRecommendation({
+  recommendation,
+}: {
+  recommendation: Recommendation;
+}) {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-2">
+        <div>
+          <BookIcon size={26} />
+        </div>
+        <div>
+          <span className="font-bold">{recommendation.title}</span>
+          <span> by </span>
+          <span className="font-bold">{recommendation.author}</span>
+        </div>
+      </div>
+      <div className="flex gap-2">
+        <div>
+          <SparklesIcon size={26} />
+        </div>
+        <div>
+          <span className="font-bold">
+            {Math.floor(recommendation.confidenceScore * 100)}%
+          </span>
+          <span> match</span>
+        </div>
+      </div>
+      <div className="flex gap-2 min-w-fit">
+        <div>
+          <MessageCircleQuestionIcon size={26} />
+        </div>
+        <div>
+          <span className="font-bold">Why:</span>{' '}
+          {recommendation.explanationToReader}
+        </div>
+      </div>
+    </div>
+  );
+}
