2e52b11a30bf372f953cd66ea0459c74d1b25521
Dylan Smith <dylantsmith@gmail.com>
2024-10-24 16:35:10 -0500

    chore: config updates
    - add a type to the config to ensure we get expected values
    - simplify the `shouldUseFakeResponses` to evaluate to a boolean, so we can avoid checking in the code

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,24 @@
-const config = {
+import { LevelWithSilent } from 'pino';
+
+export type Config = {
+  auth: {
+    cookieName: string;
+    saltRounds: number;
+  };
+  log: {
+    level: LevelWithSilent;
+  };
+  openai: {
+    apiKey: string;
+    maxTokens: number;
+    model: string;
+  };
+  prompts: {
+    shouldUseFakeResponses: boolean;
+  };
+};
+
+const config: Config = {
   auth: {
     cookieName: 'userAuth',
     saltRounds: 10,
@@ -7,15 +27,17 @@ const config = {
     level: 'trace',
   },
   openai: {
-    apiKey: process.env.OPENAI_API_KEY,
-    maxTokens: process.env.OPENAI_MAX_TOKENS || 4096,
+    apiKey: process.env.OPENAI_API_KEY!,
+    maxTokens: process.env.OPENAI_MAX_TOKENS
+      ? Number(process.env.OPENAI_MAX_TOKENS)
+      : 4096,
     model: process.env.OPENAI_MODEL || 'gpt-4o-mini-2024-07-18',
   },
   prompts: {
     // This tell the Prompts to use fake responses rather than
     // connecting with the AI. Useful for dev/testing purposes.
     // defaults to false, set to 'true' to override
-    useFakeResponses: process.env.PROMPTS_USE_FAKE_RESPONSES,
+    shouldUseFakeResponses: process.env.PROMPTS_USE_FAKE_RESPONSES === 'true',
   },
 };
 
