3d3ab258e3f2543f39d055b466ce043c03ba606e
Dylan Smith <dylantsmith@gmail.com>
2025-02-06 14:18:24 -0600

    feat: BookRecommendationComponent refactor
    We'll soon have the ability to review authors, so to prepare, merge the book recommendation components to be a single component which accepts a recommendation and a book review. Remove the unnecessary wrapper component and type.

--- src/types/BookRecommendationHydratedWithReview.ts
+++
@@ -1,8 +0,0 @@
-import BookRecommendationHydrated from '@/types/BookRecommendationHydrated';
-import BookReview from '@/types/BookReview';
-
-type BookRecommendationHydratedWithReview = BookRecommendationHydrated & {
-  bookReview?: BookReview;
-};
-
-export default BookRecommendationHydratedWithReview;
